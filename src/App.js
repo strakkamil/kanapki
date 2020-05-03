9ceeab4b3d228725f0e5f7f25155de8fbf1d0f67
Kamil <strakkamil@protonmail.com>
2020-05-03 22:19:05 +0200

    add rwd to order and main places

--- src/App.js
+++ src/App.js
@@ -13,36 +13,12 @@ import OrderNow from './components/OrderNow/OrderNow'
 import MainPlaces from './components/MainPlaces/MainPlaces';
 import './App.css'
 
-
 class App extends Component {
   state = {
     isLoading: true,
     scrollYPosition: 0,
     menuIsOpen: false,
-    heroImage: 'hero1',
-    mainPlaces: [
-      {
-        city: 'Radom',
-        street: 'Kusocińskiego 8',
-        conjunction: 'przy',
-        place: 'II  L.O. im. M. Konopnickiej',
-        link: 'https://www.google.com/maps/place/Janusza+Kusoci%C5%84skiego+8,+26-609+Radom/@51.4097875,21.1556631,18z/data=!3m1!4b1!4m5!3m4!1s0x47185ed47c8c0719:0xafcb22c42f2b4be5!8m2!3d51.4097861!4d21.1565833'
-      },
-      {
-        city: 'Radom',
-        street: 'Wernera 22',
-        conjunction: 'przy',
-        place: 'ZSE w Radomiu',
-        link: 'https://www.google.com/maps/place/Wernera+22,+26-610+Radom/@51.411079,21.1352686,17z/data=!3m1!4b1!4m8!1m2!2m1!1sradom+wernera+22!3m4!1s0x4718592f64e3951d:0x554a2e7dd674e6a5!8m2!3d51.411077!4d21.13657'
-      },
-      {
-        city: 'Radom',
-        street: 'Mariacka 25',
-        conjunction: 'przy',
-        place: 'IV L.O. im dra T. Chałubińskiego',
-        link: 'https://www.google.com/maps/place/Mariacka+25,+26-612+Radom/@51.3961211,21.1401374,17z/data=!3m1!4b1!4m8!1m2!2m1!1smariacka+25!3m4!1s0x47185ecaa52f2187:0xd26722373bf08e8a!8m2!3d51.3961178!4d21.1423261'
-      }
-    ]
+    heroImage: 'hero1'
   }
 
   componentDidMount() {
@@ -85,7 +61,7 @@ class App extends Component {
       this.setState({
         heroImage: hero[i]
       })
-    }, 7000)
+    }, 12000)
   }
 
   handleOpenMap = () => {
@@ -101,7 +77,7 @@ class App extends Component {
   }
 
   render() {
-    const { isLoading, scrollYPosition, menuIsOpen, heroImage, mainPlaces } = this.state
+    const { isLoading, scrollYPosition, menuIsOpen, heroImage } = this.state
     return (
       <Router>
         <div className='app'>
@@ -134,8 +110,8 @@ class App extends Component {
             <History />
             <Sandwiches />
             <OrderNow />
+            <h4>Punkty firmowe</h4>
             <MainPlaces
-              mainPlaces={mainPlaces}
               handleOpenPlaceMap={this.handleOpenPlaceMap}
             />
           </Route>
